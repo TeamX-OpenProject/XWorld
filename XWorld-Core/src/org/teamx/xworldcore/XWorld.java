2e6bd24dbd786b2aa1a37eb8866e31a0428b1737
lusu007 <lukas.jost@outlook.com>
2016-02-16 18:15:18 +0100

    Created basic structure

--- XWorld-Core/src/org/teamx/xworldcore/XWorld.java
+++ XWorld-Core/src/org/teamx/xworldcore/XWorld.java
@@ -7,4 +7,13 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class XWorld extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
