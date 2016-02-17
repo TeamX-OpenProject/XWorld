4d6c639c67ff6e62a79ad5a981f92f42de7034d2
ELaFYT <justin.wiese2001@gmail.com>
2016-02-17 16:01:49 +0100

    Starting with the api

--- XWorld-Core/src/org/teamx/xworldcore/XWorld.java
+++ XWorld-Core/src/org/teamx/xworldcore/XWorld.java
@@ -3,7 +3,9 @@ package org.teamx.xworldcore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Created by lusu007 on 16.02.2016.
+ * @author lusu007
+ * @author Shustin
+ *
  */
 public class XWorld extends JavaPlugin {
 
