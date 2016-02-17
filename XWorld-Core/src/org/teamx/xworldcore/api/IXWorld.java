bb2615aa4c9b1184b1e22adc1e1ea8d8ec53b62a
ELaFYT <justin.wiese2001@gmail.com>
2016-02-17 15:53:13 +0100

    Starting with the api

---
+++ XWorld-Core/src/org/teamx/xworldcore/api/IXWorld.java
@@ -0,0 +1,88 @@
+package org.teamx.xworldcore.api;
+
+import org.bukkit.Difficulty;
+import org.bukkit.GameMode;
+import org.bukkit.World;
+import org.bukkit.WorldType;
+
+/**
+ * @World-Properties
+ * @author Shustin
+ */
+
+public abstract interface IXWorld {
+
+    /**
+     *
+     * @return world
+     */
+
+    public abstract World getXWorld();
+
+    /**
+     *
+     * @return WorldName
+     */
+
+    public abstract String getWorldName();
+
+    /**
+     *
+     * @return difficulty
+     */
+
+    public abstract Difficulty getDifficulty();
+
+    /**
+     *
+     * @param difficulty
+     * @return
+     */
+
+    public abstract void setDifficulty(Difficulty difficulty);
+
+    /**
+     *
+     * @return seed
+     */
+
+    public abstract long getSeed();
+
+    /**
+     *
+     * @param seed
+     * @return
+     */
+
+    public abstract void setSeed(long seed);
+
+    /**
+     *
+     * @return worldType
+     */
+
+    public abstract WorldType getWorldType();
+
+    /**
+     *
+     * @param worldType
+     * @return
+     */
+
+    public abstract void setWorldType(WorldType worldType);
+
+    /**
+     *
+     * @return gamemode
+     */
+
+    public abstract GameMode getGamemode();
+
+    /**
+     *
+     * @param gameMode
+     * @return
+     */
+
+    public abstract void setGameMode(GameMode gameMode);
+}
