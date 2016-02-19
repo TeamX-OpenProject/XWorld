100ae358af5c78376ba8be64eb27d9ee6e4c7edc
ELaFYT <justin.wiese2001@gmail.com>
2016-02-19 23:42:54 +0100

    api update

--- XWorld-Core/src/org/teamx/xworldcore/api/entities/IPermissionHolder.java
+++ XWorld-Core/src/org/teamx/xworldcore/api/entities/IPermissionHolder.java
@@ -1,7 +1,12 @@
 package org.teamx.xworldcore.api.entities;
 
+import org.bukkit.permissions.Permission;
+
 /**
  * @author Shustin
  */
 public interface IPermissionHolder {
+
+    void hasPermission(Permission permission);
+
 }
